32dccf2818438f42434e70e0d30f2804ae460309
Nguyễn Đình Tâm <tamnd12@viettel.com.vn>
2019-10-25 14:57:44 +0700

    tamnd12: fix missing err variables in user services

--- backend-webUI/server/services/user-services.js
+++ backend-webUI/server/services/user-services.js
@@ -4,23 +4,23 @@
  * @date 23/10/2019
  */
 'use strict';
-const { User } 	    = require('../models/mysql-connector');
-const validator     = require('validator');
-const { to, ThrowErr }    = require('./util-services');
+const { User } = require('../models/mysql-connector');
+const validator = require('validator');
+const { to, ThrowErr} = require('./util-services');
 
 /**
  * @Des This function return key from body
  * @param {Object} body : req body will include email or phone, password
  * @return {} key : unique key
  */
-const getKeyFromBody = function(body){
+const getKeyFromBody = (body) => {
     let key = body.unique_key;
-    if(typeof key==='undefined'){
-        if(typeof body.email != 'undefined'){
-            key = body.email
-        }else if(typeof body.phone != 'undefined'){
-            key = body.phone
-        }else{
+    if (typeof key === 'undefined') {
+        if (typeof body.email != 'undefined') {
+            key = body.email;
+        } else if (typeof body.phone != 'undefined') {
+            key = body.phone;
+        } else {
             key = null;
         }
     }
@@ -31,35 +31,42 @@ const getKeyFromBody = function(body){
  * @param {Object} UserInfo : req body will include email or phone, password
  * @return {Object} user : authen user from Database if true
  */
-const authUser = async function(UserInfo){ 
+const authUser = async (userInfo) => {
     let unique_key;
     let auth_info = {};
     auth_info.status = 'login';
-    key = getKeyFromBody(userInfo);
+    unique_key = getKeyFromBody(userInfo);
+    console.log(userInfo);
     //Check Email or Phone
-    if(!unique_key) ThrowErr('Please enter an email or phone number to login');
+    if (!unique_key) ThrowErr('Please enter an email or phone number to login');
     //Check password has fill?
-    if(!userInfo.password) ThrowErr('Please enter a password to login');
+    if (!userInfo.password) ThrowErr('Please enter a password to login');
     //Check user use email or phone 
-    let user;
-    if(validator.isEmail( key )){
+    let user, err;
+    if (validator.isEmail(unique_key)) {
         auth_info.method = 'email';
-        [err, user] = await to(User.findOne({where:{email:key}}));
-        //If email not found in Db
-        if(err) ThrowErr(err.message);
-    } else if( validator.isMobilePhone(unique_key, 'any')){ 
-        auth_info.method='phone';
-        [err, user] = await to(User.findOne({where:{phone:key }}));
-        //If mobile phone not found in Db
-        if(err) ThrowErr(err.message);
-    } else{
+
+        [err, user] = await to(User.findOne({
+            where: {
+                email: unique_key
+            }
+        }));
+        if (err) ThrowErr(err.message); //If email not found in Db
+        
+    } else if (validator.isMobilePhone(unique_key, 'any')) {
+        [err, user] = await to(User.findOne({
+            where: {
+                email: unique_key
+            }
+        }));
+        if (err) ThrowErr(err.message); //If mobile phone not found in Db
+    } else {
         ThrowErr('A valid email or phone number was not entered');
     }
-    if(!user) ThrowErr('Not registered');
+    if (!user) ThrowErr('Not registered');
     //Call function to check password
     [err, user] = await to(user.comparePassword(userInfo.password));
-    if(err) ThrowErr(err.message);
-    //Return user info
-    return user;
+    if (err) ThrowErr(err.message);
+    return user; //Return user info
 }
 module.exports.authUser = authUser;
